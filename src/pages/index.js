cf9e6c0ec8a045f15eccdb5ef7791c74f7aa202f
maureenlholland <maureen.holland11@gmail.com>
2020-06-28 15:39:26 -0400

    add post queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
 
-export default () => (
-    <Layout>
-        <h1>Home</h1>
-        <p>Gatsby Intro Demo</p>
-        <Link to="/about">About page</Link>
-    </Layout>
-);
+export default () => {
+    const posts = usePosts();
+
+    return (
+        <Layout>
+            <h1>Home</h1>
+            <p>Gatsby Intro Demo</p>
+            <Link to="/about">About page</Link>
+
+            <h2>Read my blog</h2>
+            {posts.map((post) => (
+                <PostPreview
+                    key={post.slug}
+                    post={post}
+                />
+            ))}
+        </Layout>
+    );
+}
