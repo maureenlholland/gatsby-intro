44669157134490a7bedc828c1482376c0ae4657e
maureenlholland <maureen.holland11@gmail.com>
2020-06-28 21:01:27 -0400

    Add analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
                     default: require.resolve('./src/components/layout.js')
                 },
                 gatsbyRemarkPlugins: ['gatsby-remark-images'],
-                plugins: [{ resolve: 'gatsby-remark-images' }],
+                plugins: ['gatsby-remark-images'],
             }
         },
         {
@@ -30,6 +30,15 @@ module.exports = {
             options: {
                 name: 'images',
                 path: 'images'
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+            options: {
+                production: true,
+                disable: !process.env.ANALYZE_BUNDLE_SIZE,
+                generateStatsFile: true,
+                analyzerMode: 'static',
             }
         }
     ],
